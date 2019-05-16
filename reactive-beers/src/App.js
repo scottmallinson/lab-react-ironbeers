f3f81fcfcf4970be8afc60536e8e996fe1f91af6
Scott Mallinson <scott@scottmallinson.com>
2019-05-16 19:52:34 +0200

    Added navigation

--- reactive-beers/src/App.js
+++ reactive-beers/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import Header from './components/Header'
 import Beers from './components/Beers'
+import Beer from './components/Beer'
 import RandomBeer from './components/RandomBeer'
 import NewBeer from './components/NewBeer'
 
@@ -10,8 +11,12 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Link to="/beers">All beers</Link>
+      <Link to="/random-beer">Random beer</Link>
+      <Link to="/new-beer">New beer</Link>
       <Switch>
         <Route exact path="/beers" component={Beers} />
+        <Route exact path="/beers/:beerid" component={Beer} />
         <Route exact path="/random-beer" component={RandomBeer} />
         <Route exact path="/new-beer" component={NewBeer} />
       </Switch>
--- reactive-beers/src/components/Beers.js
+++ reactive-beers/src/components/Beers.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 export default class Beers extends Component {
   constructor(){
@@ -22,7 +23,7 @@ export default class Beers extends Component {
   render() {
     return (
       <div>
-        {this.state.beers.map(beer => <div><h1>{beer.name}</h1><img src={beer.image_url} alt='' /><p>{beer.tagline}</p><p>{beer.contributed_by}</p></div>)}
+        {this.state.beers.map(beer => <div><Link to={`/beers/${beer._id}`}><h1>{beer.name}</h1><img src={beer.image_url} alt='' /><p>{beer.tagline}</p><p>{beer.contributed_by}</p></Link></div>)}
       </div>
     )
   }
